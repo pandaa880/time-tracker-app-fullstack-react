af2f45ec68721979a483969fde29bd439517504d
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-11-24 21:20:00 +0530

    add CRUD functionality

--- src/components/EditableTimer.js
+++ src/components/EditableTimer.js
@@ -1,25 +1,60 @@
-import React from "react"
+import React, { useState } from "react"
 
 import Timer from "./Timer"
 import TimerForm from "./TimerForm"
 
 function EditableTimer({
-  editFormOpen,
+  id,
   title,
   project,
   elapsed,
-  runningSince
+  runningSince,
+  onFormSubmit,
+  onDeleteClick
 }) {
+  const [editFormOpen, setEditFormOpen] = useState(false)
+
+  /** handle form open */
+  const handleEditClick = () => {
+    setEditFormOpen(true)
+  }
+
+  /** handle form close */
+  const handleFormClose = () => {
+    setEditFormOpen(false)
+  }
+
+  /** handle form submit */
+  const handleFormSubmit = updatedValue => {
+    onFormSubmit(updatedValue)
+    setEditFormOpen(false)
+  }
+
+  /** handle delete timer click */
+  const handleDeleteClick = () => {
+    onDeleteClick(id)
+    setEditFormOpen(false)
+  }
+
   return (
     <>
       {editFormOpen ? (
-        <TimerForm title={title} project={project} />
+        <TimerForm
+          id={id}
+          title={title}
+          project={project}
+          onFormClose={handleFormClose}
+          onFormSubmit={handleFormSubmit}
+        />
       ) : (
         <Timer
+          id={id}
           title={title}
           project={project}
           elapsed={elapsed}
           runningSince={runningSince}
+          onEditClick={handleEditClick}
+          onDeleteClick={handleDeleteClick}
         />
       )}
     </>
