f9a32e1976ddaf7bc591f4ee0e48619df5ec49ab
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-11-24 21:21:06 +0530

    add functionality to create a new timer

--- src/components/ToggleableTimerForm.js
+++ src/components/ToggleableTimerForm.js
@@ -1,7 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
+import { FaPlus } from "react-icons/fa"
 
-function ToggleableTimerForm() {
-  return <div>Toggleable Timer Form</div>
+import TimerForm from "./TimerForm"
+
+import styles from "../styles/ToggleableTimerForm.module.css"
+
+function ToggleableTimerForm(props) {
+  /** set state */
+  const [isTimerFormOpen, setIsTimerFormOpen] = useState(false)
+
+  /** handle form submit */
+  const handleFormSubmit = timer => {
+    props.onFormSubmit(timer)
+    setIsTimerFormOpen(false)
+  }
+
+  return (
+    <div className={styles.container}>
+      {isTimerFormOpen ? (
+        <TimerForm
+          onFormClose={() => setIsTimerFormOpen(false)}
+          onFormSubmit={handleFormSubmit}
+        />
+      ) : (
+        <div className={styles.buttonContainer}>
+          <button
+            className={styles.button}
+            onClick={() => setIsTimerFormOpen(true)}
+          >
+            <FaPlus className={styles.toggleIcon} />
+          </button>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default ToggleableTimerForm
