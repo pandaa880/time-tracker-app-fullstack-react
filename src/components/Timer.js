d4eda727cb5011a7eef76832411c9e2c3dc8893e
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-11-24 22:33:10 +0530

    make a component for start and stop button and add rerender using forceupdate

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,39 +1,72 @@
 import React from "react"
 import { FaRegEdit, FaTrashAlt } from "react-icons/fa"
 
+import TimerActionButton from "./TimerActionButton"
+
 import { renderElapsedString } from "../helpers"
 
 import styles from "../styles/Timer.module.css"
 
-function Timer({ title, project, elapsed, onEditClick, onDeleteClick }) {
-  const elapsedString = renderElapsedString(elapsed)
+class Timer extends React.Component {
+  /** lifecycle methods */
+  componentDidMount() {
+    this.forceIntervalUpdate = setInterval(() => this.forceUpdate(), 50)
+  }
 
-  return (
-    <div className={styles.container}>
-      <h3 className={styles.titleText}>{title}</h3>
-      <h4 className={styles.projectText}>{project}</h4>
-      <h2 className={styles.elapsedTime}>{elapsedString}</h2>
-      <div className={styles.buttonsContainer}>
-        <button
-          type="button"
-          className={styles.editButton}
-          onClick={onEditClick}
-        >
-          <FaRegEdit className={styles.editIcon} />
-        </button>
-        <button
-          type="button"
-          className={styles.deleteButton}
-          onClick={onDeleteClick}
-        >
-          <FaTrashAlt className={styles.deleteIcon} />
-        </button>
+  componentWillUnmount() {
+    clearInterval(this.forceIntervalUpdate)
+  }
+
+  /** handle start and stop timer click */
+  handleStartClick = () => {
+    this.props.onStartClick(this.props.id)
+  }
+
+  handleStopClick = () => {
+    this.props.onStopClick(this.props.id)
+  }
+
+  render() {
+    const {
+      title,
+      project,
+      elapsed,
+      runningSince,
+      onEditClick,
+      onDeleteClick
+    } = this.props
+
+    const elapsedString = renderElapsedString(elapsed, runningSince)
+
+    return (
+      <div className={styles.container}>
+        <h3 className={styles.titleText}>{title}</h3>
+        <h4 className={styles.projectText}>{project}</h4>
+        <h2 className={styles.elapsedTime}>{elapsedString}</h2>
+        <div className={styles.buttonsContainer}>
+          <button
+            type="button"
+            className={styles.editButton}
+            onClick={onEditClick}
+          >
+            <FaRegEdit className={styles.editIcon} />
+          </button>
+          <button
+            type="button"
+            className={styles.deleteButton}
+            onClick={onDeleteClick}
+          >
+            <FaTrashAlt className={styles.deleteIcon} />
+          </button>
+        </div>
+        <TimerActionButton
+          timerIsRunning={!!runningSince}
+          onStartClick={this.handleStartClick}
+          onStopClick={this.handleStopClick}
+        />
       </div>
-      <button type="button" className={styles.startTimerButton}>
-        Start
-      </button>
-    </div>
-  )
+    )
+  }
 }
 
 export default Timer
