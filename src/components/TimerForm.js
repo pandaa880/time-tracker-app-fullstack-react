d1d281b2a78d9bc4f74527002a7396c58a5a7dc5
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-11-24 21:20:38 +0530

    add functionality to update the values

--- src/components/TimerForm.js
+++ src/components/TimerForm.js
@@ -1,17 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 
 import styles from "../styles/TimerForm.module.css"
 
-function TimerForm({ title, project }) {
+function TimerForm(props) {
+  /** set state */
+  const [title, setTitle] = useState(props.title || "")
+  const [project, setProject] = useState(props.project || "")
+
   // submit button text
-  const submitButtonText = title ? "Update" : "Create"
+  const submitButtonText = props.id ? "Update" : "Create"
+
+  /** handle form submit */
+  const handleFormSubmit = e => {
+    // stop default behavior
+    e.preventDefault()
+    // pass all the required values to function from parent
+    props.onFormSubmit({
+      id: props.id,
+      title,
+      project
+    })
+  }
 
   return (
     <div className={styles.formContainer}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleFormSubmit}>
         <div className={styles.formGroup}>
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" defaultValue={title} />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
         </div>
         <div className={styles.formGroup}>
           <label htmlFor="project">Project</label>
@@ -19,12 +41,25 @@ function TimerForm({ title, project }) {
             type="text"
             name="project"
             id="project"
-            defaultValue={project}
+            value={project}
+            onChange={e => setProject(e.target.value)}
           />
         </div>
         <div className={styles.formButtonsContainer}>
-          <button className={styles.submitButton}>{submitButtonText}</button>
-          <button className={styles.cancelButton}>Cancel</button>
+          <button
+            type="submit"
+            className={styles.submitButton}
+            onClick={handleFormSubmit}
+          >
+            {submitButtonText}
+          </button>
+          <button
+            type="button"
+            className={styles.cancelButton}
+            onClick={props.onFormClose}
+          >
+            Cancel
+          </button>
         </div>
       </form>
     </div>
