3aac1572845e4190c6876c6221a60f7f2e2b14c1
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2019-11-24 21:15:58 +0530

    pass CRUD functions to EditableTimer

--- src/components/EditableTimersList.js
+++ src/components/EditableTimersList.js
@@ -5,23 +5,27 @@ import EditableTimer from "./EditableTimer"
 // import styles
 import styles from "../styles/EditableTimersList.module.css"
 
-function EditableTimersList() {
+function EditableTimersList({ timers, onFormSubmit, onDeleteClick }) {
   return (
     <div className={styles.timersList}>
-      <EditableTimer
-        title="Learn React"
-        project="React 101"
-        elapsed="8986300"
-        runningSince={null}
-        editFormOpen={false}
-      />
-      <EditableTimer
-        title="Learn GraphQl"
-        project="React with GraphQl"
-        elapsed="3890985"
-        runningSince={null}
-        editFormOpen={true}
-      />
+      {timers && timers.length > 0 ? (
+        timers.map(timer => (
+          <EditableTimer
+            key={timer.id}
+            id={timer.id}
+            title={timer.title}
+            project={timer.project}
+            elapsed={timer.elapsed}
+            runningSince={timer.runningSince}
+            onFormSubmit={onFormSubmit}
+            onDeleteClick={onDeleteClick}
+          />
+        ))
+      ) : (
+        <h3 className={styles.noTimersText}>
+          No Timers.. <br /> Add by clicking a + button below.
+        </h3>
+      )}
     </div>
   )
 }
